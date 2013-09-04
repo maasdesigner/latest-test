23cd680e134aec725e5981caed47c1acbc40d29a
maasdesigner <maasgultekin@gmail.com>
2013-09-04 21:59:08 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metroson::Application.config.secret_token = 'c1883b1e74cc27e8f0b9941f1ecd18c98c0df763fa767c9d769db352b750a151a8b015e9e696a916403dd4d4fd76ac1fad07cc231038f3541b93f4f18de9dd09'
